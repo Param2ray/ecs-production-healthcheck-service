d052d40d74f084e74b69c28ad4998daf8105f3a8
Param2ray <param_2ray@hotmail.co.uk>
2026-01-06 22:19:15 -0500

    Add ECS and VPC module wiring

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,30 @@
 module "ecs" {
-    source = "./modules/ecs"
+  source = "./modules/ecs"
 
-    cluster_name = var.cluster_name
-    ecs_launch_type = var.ecs_launch_type
-    desired_count = var.desired_count
-    container_port = var.container_port
-    cpu = var.cpu
-    memory = var.memory
-    vpc_id = module.vpc.vpc_id
-    image_url = var.image_url
-    http_listener_arn = module.alb.http_listener_arn
-    https_listener_arn = module.alb.https_listener_arn
-    iam_role_arn = module.iam.iam_role_arn
-    private_subnet_ids = module.vpc.private_subnet_ids
-    target_group_arn = module.alb.target_group_arn
-    alb_security_group_id = module.alb.alb_security_group_id
+  cluster_name          = var.cluster_name
+  ecs_launch_type       = var.ecs_launch_type
+  desired_count         = var.desired_count
+  container_port        = var.container_port
+  cpu                   = var.cpu
+  memory                = var.memory
+  vpc_id                = module.vpc.vpc_id
+  image_url             = var.image_url
+  http_listener_arn     = module.alb.http_listener_arn
+  https_listener_arn    = module.alb.https_listener_arn
+  iam_role_arn          = module.iam.iam_role_arn
+  private_subnet_ids    = module.vpc.private_subnet_ids
+  target_group_arn      = module.alb.target_group_arn
+  alb_security_group_id = module.alb.alb_security_group_id
+
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  name                 = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  az_count             = var.az_count
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+
 }
